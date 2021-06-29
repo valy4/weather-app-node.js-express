c11fba6568b704f89192e2bc9c45ba7d2702da55
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-06-29 17:25:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const https = require('https');
+const express = require('express')
+const app = express()
+
+app.get("/", function (request, res) {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=7a2c0c8ca6f285d85a21f43ecc92f829&units=metric"
+  https.get(url, function (response) {
+    console.log(response.statusCode)
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data)
+      const icon = weatherData.weather[0].icon
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      console.log(weatherDescription)
+      res.write("<p>The weather is currently " + weatherDescription + "</p>")
+      res.write("<h1> The temperature is " + temp + " degrees Celcius. </h1>")
+      res.write("<img src=" + imageUrl + " />")
+      res.send()
+    })
+  })
+  // response.send("server is up and running")
+})
+
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`)
+})
